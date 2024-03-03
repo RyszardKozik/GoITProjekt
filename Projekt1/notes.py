cba83a2f05c8e9cd8d001d77afb979278d1f212f
pawelzajac95 <pawelzajac34@wp.pl>
2024-03-03 14:18:41 +0100

    modyfikacja kodu usuwania notatki, rozpoczynajac od ID 1 nie od ID 0

--- Projekt1/notes.py
+++ Projekt1/notes.py
@@ -3,6 +3,7 @@ import pickle
 from datetime import datetime
 from tag_manager import TagManager
 
+
 class Note:
     def __init__(self, title, content, tags=None):
         self.title = title
@@ -12,6 +13,7 @@ class Note:
 
     def __str__(self):
         return f"{self.created_at}: {self.title} - {self.content} - Tagi: {', '.join(self.tags)}"
+
 
 class Notebook:
     def __init__(self):
@@ -32,14 +34,17 @@ class Notebook:
             print("Brak notatek do wyświetlenia.")
             return
         for note in self.notes:
-            print(f"Tytuł: {note.get('title', 'Brak tytułu')}\nTreść: {note.get('content', 'Brak treści')}\nTagi: {', '.join(note.get('tags', []))}")
+            print(f"Tytuł: {note.get('title', 'Brak tytułu')}\nTreść: {
+                  note.get('content', 'Brak treści')}\nTagi: {', '.join(note.get('tags', []))}")
 
     def delete_note(self, note_id):
         try:
-            note_id = int(note_id) # Konwersja na int
-            if 0 <= note_id < len(self.notes):
-                deleted_note = self.notes.pop(note_id) # Usuwa notatkę i zwraca ją
-                print(f"Usunięto notatkę: {deleted_note.get('title', 'Brak tytułu')}")
+            note_id = int(note_id)  # Konwersja na int
+            if 0 < note_id <= len(self.notes):
+                # Usuwa notatkę i zwraca ją
+                deleted_note = self.notes.pop(note_id-1)
+                print(f"Usunięto notatkę: {
+                      deleted_note.get('title', 'Brak tytułu')}")
             else:
                 print("Notatka o podanym ID nie istnieje.")
         except ValueError:
@@ -68,19 +73,20 @@ class Notebook:
         # Przekształcanie tagów dla każdej notatki na sety, aby umożliwić efektywne sprawdzanie
         for note in self.notes:
             note['tags'] = set(note['tags'])
-        
+
         # Teraz wyszukujemy notatki, które zawierają tag w zestawie tagów
         found_notes = [note for note in self.notes if tag in note['tags']]
-        
+
         if not found_notes:
             print("Nie znaleziono notatek z podanym tagiem.")
             return
-        
+
         for note in found_notes:
             print()
             print("Oto twoja notatka:")
             print()
-            print(f"Tytuł: {note['title']}\nTreść: {note['content']}\nTagi: {', '.join(note['tags'])}")
+            print(f"Tytuł: {note['title']}\nTreść: {
+                  note['content']}\nTagi: {', '.join(note['tags'])}")
 
     def save_notes(self, filename='notes.pkl'):
         try:
@@ -101,14 +107,14 @@ class Notebook:
         except Exception as e:
             print(f"Błąd przy wczytywaniu notatek: {e}")
 
-    
     def sort_notes_by_tags(self, tag):
         """Sortuje notatki według liczby wystąpień wprowadzonego tagu."""
         # Sprawdzenie czy tag istnieje w notatkach
         tag_exists = any(tag in note['tags'] for note in self.notes)
         if not tag_exists:
-            return False # Tag nie istnieje, zwróć False
+            return False  # Tag nie istnieje, zwróć False
 
         # sortowanie notatki według liczby wystąpień tagu
-        self.notes.sort(key=lambda note: list(note['tags']).count(tag), reverse=True)
+        self.notes.sort(key=lambda note: list(
+            note['tags']).count(tag), reverse=True)
         return True
